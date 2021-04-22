6473ae8ca7a9de82b7d5cf509130063fa18d9571
Vicki <vicki.langer@statalog.org>
2021-04-22 16:00:26 -0400

    try regular for loop instead of forEach

--- hunt.js
+++ hunt.js
@@ -19,14 +19,14 @@ function fill_prompts() {
 
     // show item in <li> tag
     hunt_prompts = document.querySelectorAll(".hunt-prompt") // get all elements
-    hunt_prompts.forEach(element => {
+    for (prompt in hunt_prompts) {
         var length = prompts.length // get current length of array
         var rand = Math.round(Math.random() * length)  // get rand num w/ decimal, then rounds to get rid of the decimal
-        element.innerHTML = prompts[rand]; // change el to random index in array
+        hunt_prompts.innerHTML = prompts[rand]; // change el to random index in array
         //prompts.splice(rand); //remove index after use //TODO:make sure not to use a prompt more than once in the list of 10
         //element.style.background = "orange"; // just for testing
         console.log("<li> updated") // just for testing
-    });
+    };
 
 }
 
