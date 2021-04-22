214fc6d6ff6f10cc67008335fa08171105aedcb5
Vicki <vicki.langer@statalog.org>
2021-04-22 16:02:47 -0400

    move length outside of func

--- hunt.js
+++ hunt.js
@@ -17,10 +17,12 @@ function get_prompts() {
 function fill_prompts() {
     get_prompts();
 
+    var length = prompts.length // get current length of array
+
     // show item in <li> tag
     hunt_prompts = document.querySelectorAll(".hunt-prompt") // get all elements
     for (prompt in hunt_prompts) {
-        var length = prompts.length // get current length of array
+
         var rand = Math.round(Math.random() * length)  // get rand num w/ decimal, then rounds to get rid of the decimal
         hunt_prompts.innerHTML = prompts[rand]; // change el to random index in array
         //prompts.splice(rand); //remove index after use //TODO:make sure not to use a prompt more than once in the list of 10
