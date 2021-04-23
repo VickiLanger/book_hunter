0d314e2388700e26c913e6da67b5e1687e1c1313
Vicki <vicki.langer@statalog.org>
2021-04-23 13:01:12 -0400

    populate actual entry instead of index position

--- hunt.js
+++ hunt.js
@@ -10,8 +10,7 @@ function get_prompts() {
             lines = data.split('\n')// split into strings by line
             console.log("lines " + lines);  //for testing
             for (line in lines) {
-                //string(line);  //make each line into a string
-                prompts.push(line);  // populate array with individual lines from txt file
+                prompts.push(lines[line]);  // populate array with individual lines from txt file
                 console.log("line added to array, hopefully")
             }
             console.log(prompts);  //for testing
